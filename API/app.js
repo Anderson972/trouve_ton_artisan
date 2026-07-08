002e3d1db31ab08bb798efc39c1e1a62b2ddbb95
Anderson LUCE <anderson.luce@gmail.com>
2026-07-08 13:15:34 +0200

    add controller and routes /categories + app.js

---
+++ API/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const artisansRoutes = require('./src/routes/artisans');
+const categoriesRoutes = require('./src/routes/categories');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/artisans', artisansRoutes);
+app.use('/categories', categoriesRoutes);
+
+
+
+module.exports = app;
--- API/src/controllers/artisans.js
+++ API/src/controllers/artisans.js
@@ -1,19 +1,38 @@
 const {Artisan, Specialite, Categorie} = require ('../models')
-
+const { Op } = require('sequelize');
 
 
 exports.getAll = async (req, res) => {
 
-try {
-   const artisans =  await Artisan.findAll({include: {
-        model: Specialite,
-        include: Categorie
-    }});
-    return res.status(200).json(artisans)
-} catch (error) {
-    console.error("erreur lors de la recuperation des artisans", error)
-    return res.status(500).json({message : "erreur serveur artisans"})
-}
+    const filtre = {}
+    const filtre_cat = {}
+    try {
+
+    if(req.query.id_specialite){
+        filtre.fk_specialite_artisans = req.query.id_specialite
+    }
+    if(req.query.nom){
+        filtre.nom = {[Op.like]:'%'+req.query.nom+'%'} 
+    }
+    if(req.query.id_categories){
+        filtre_cat.fk_categories_specialite = req.query.id_categories
+    }
+
+    const includeSpecialite = {model : Specialite, include: Categorie}
+    if (Object.keys(filtre_cat).length>0) {
+        includeSpecialite.where = filtre_cat
+    }
+
+
+    const artisans =  await Artisan.findAll({
+        where: filtre,
+        include: includeSpecialite
+    });
+        return res.status(200).json(artisans)
+    } catch (error) {
+        console.error("erreur lors de la recuperation des artisans", error)
+        return res.status(500).json({message : "erreur serveur artisans"})
+    }
 };
 
 exports.getById = async (req, res) => {
