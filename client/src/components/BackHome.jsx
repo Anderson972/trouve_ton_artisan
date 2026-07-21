76a72cbbcc9a6768d2593a18882d15da84000cc7
Anderson972 <anderson.luce@gmail.com>
2026-07-21 20:58:15 +0200

    Update BackHome.jsx
    ajustement style bouton retour à l'acceuil

--- client/src/components/BackHome.jsx
+++ client/src/components/BackHome.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const BackHome = () => {
 
 
     return(
-        <div className="back-home p-3 mt-3">
-            <button className="rounded-pill btn btn-primary px-4">Retourer à la page d'acceuil <i className="bi bi-arrow-right"></i></button>
+        <div className="back-home p-3 mt-3" >
+            <button className="rounded-pill btn btn-primary px-4"><Link className="text-white text-decoration-none" to={'/'}>Retourer à la page d'acceuil <i className="bi bi-arrow-right"></i></Link></button>
         </div>
     )
 };
